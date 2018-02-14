09c352b5b463109b39f8b04230ee4cbeefcc5316
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-14 05:48:39 -0800

    Create compareguvi.c

---
+++ compareguvi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char a[10]="dream";
+	char b[10]="tamilnadu";
+	int i,c,d,count=0;
+	c=strlen(a);
+	d=strlen(b);
+	for(i=0;i<d;i++)
+	{
+	    
+	    a[i]==b[i];
+	    count++;
+	}
+		if(a[i]>b[i])
+		{
+			printf("the compared string is %s",a);
+		}
+	
+		else
+		{
+			printf("the compared string is %s",b);
+		}
+		
+	
+	
+	
+}
