0ee0e90d87ca827d7459ea96ef6eed10bd3c1392
gomathisubramani <31499155+gomathisubramani@users.noreply.github.com>
2018-02-19 05:50:58 -0800

    Create ibm7.c

---
+++ ibm7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	printf("\t S.GOMATHI,\t ECE-A,\t 922515106033");
+	int year;
+	char a[10],b[10];
+	printf("enter the first string");
+	scanf("%s",a);
+	printf("enter the second string");
+	scanf("%s",b);
+	printf("enter the year");
+	scanf("%d",&year);
+	printf("\n%s\t%s\t%d",a,b,year);
+}
